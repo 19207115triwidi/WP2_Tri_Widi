b7013ad2e1eab5eebc22d55b9641c1ab21b7179a
wp2-triwidikuswanti <101647327+19207115triwidi@users.noreply.github.com>
2022-04-05 23:25:45 +0700

    Tes update

--- application/controllers/Latihansatu.php
+++ application/controllers/Latihansatu.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Latihansatu extends CI_Controller {
 
 	 public function index() {
-    echo "Selamat Datang... selamat belajar web programming";
+    echo "Selamat Datang... selamat belajar web programming 2";
   }
 
 	 public function penjumlahan($n1, $n2) {
